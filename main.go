a72312ea84a0adf614755b8b3df19597ea99ec44
Andres Orozco <ao15@my.fsu.edu>
2019-10-17 16:13:43 -0700

    main entrypoint server using graphite, add graphite prefixes

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"github.com/andresoro/hemera/pkg/backend"
+	"github.com/andresoro/hemera/pkg/server"
+)
+
 func main() {
+
+	graphite := &backend.Graphite{Addr: "localhost:2023"}
+
+	srv, err := server.New(10*time.Second, "localhost", "8484", graphite)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srv.Run()
 
 }
---
+++ pkg/backend/graphite.go
@@ -0,0 +1,83 @@
+package backend
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/andresoro/hemera/pkg/cache"
+)
+
+const (
+	PREFIX  = "hemera"
+	COUNTER = "counters"
+	GAUGE   = "gauges"
+	SET     = "sets"
+	TIMER   = "timers"
+)
+
+// Graphite implementation of backend interface
+type Graphite struct {
+	Addr string
+}
+
+// Purge implements backend interface
+func (g *Graphite) Purge(c *cache.Cache) error {
+	now := time.Now().Unix()
+
+	// concatenated buffer to hold all metrics
+	var buffer strings.Builder
+
+	for name, value := range c.Counters {
+		fullName := fmt.Sprintf("%s.%s.%s", PREFIX, COUNTER, name)
+		metric := metricString(fullName, value, now)
+		_, err := buffer.WriteString(metric)
+		if err != nil {
+			return err
+		}
+	}
+
+	for name, value := range c.Gauges {
+		fullName := fmt.Sprintf("%s.%s.%s", PREFIX, GAUGE, name)
+		metric := metricString(fullName, value, now)
+		_, err := buffer.WriteString(metric)
+		if err != nil {
+			return err
+		}
+	}
+
+	for name, value := range c.Sets {
+		fullName := fmt.Sprintf("%s.%s.%s", PREFIX, COUNTER, name)
+		key := fullName + ".count"
+		n := float64(len(value))
+
+		metric := metricString(key, n, now)
+
+		_, err := buffer.WriteString(metric)
+		if err != nil {
+			return err
+		}
+	}
+
+	// dial graphite server
+	conn, err := net.Dial("tcp", g.Addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// write string buffer
+	log.Printf("pushing %s", buffer.String())
+	_, err = conn.Write([]byte(buffer.String()))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func metricString(name string, value float64, date int64) string {
+	return fmt.Sprintf("%s %f %d \n", name, value, date)
+}
